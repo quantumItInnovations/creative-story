c096b49e3010787cb44b0bc0e402f6290f55a1bd
shobhitchoudhary745 <shobhitchoudhary745@gmail.com>
2024-04-10 15:04:12 +0530

    changes in apis

--- src/utils/axiosUtil.js
+++ src/utils/axiosUtil.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000",
-  baseURL: "https://creative-story-backend.adaptable.app",
+  // baseURL: "https://creative-story-backend.adaptable.app",
+  baseURL:"https://api.keepitgoingstory.com"
 });
 
 export default axiosInstance;

af114dc84baf2a6930e338327e6e0f55066cd8b2
Arul <arulpraveen19112002@gmail.com>
2025-06-23 20:15:48 +0530

    phase 2 url updated

--- src/utils/axiosUtil.js
+++ src/utils/axiosUtil.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // baseURL: "http://localhost:4000",
   // baseURL: "https://creative-story-backend.adaptable.app",
-  baseURL:"https://api.keepitgoingstory.com"
+  // baseURL:"https://api.keepitgoingstory.com"
+  baseURL: "https://keepitgoing-phase-2.onrender.com",
 });
 
 export default axiosInstance;
